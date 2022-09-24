4c0228acd57c397c8ad10f913ac5e19404ce6f0b
PietroDanieli <1pie7d@gmail.com>
2022-09-24 16:17:02 +0200

    initial commit: vue create vue-dc-comics and run build

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <comp_header />
+  </div>
+</template>
+
+<script>
+import comp_header from "./components/comp_header.vue";
+
+export default {
+  name: "App",
+  components: {
+    comp_header,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/comp_header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">ciao</div>
+</template>
+
+<script>
+export default {
+  name: "comp_header",
+  props: {
+    // msg: String
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
